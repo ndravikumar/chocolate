3534e7961fb650f7bdcd03d0f25e5e8852cfb859
RaviKumar <naramalaravi@gmail.com>
2024-12-24 18:25:13 +0530

    PR test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import ChocolateDetails from './components/ChocolateDetails';
 import ChocolateList from './components/ChocolateList';
 import Cart from './components/Cart';
+import Timer from './Timer';
 
 function App() {
   const [cart, setCart] = React.useState([]);
   return (
     <Router>
       <Navbar cart={cart} />
+      <Timer />
       <Routes>
         <Route path='/' element={<ChocolateList setCart={setCart} />} />
         <Route path='/chocolate-details/:id' element={<ChocolateDetails />} />
@@ -23,3 +25,10 @@ function App() {
 }
 
 export default App;
+
+
+// <BrowserRouter>
+//   <Routes>
+//     <Route path='' element={<Home />} >
+//   </Routes>
+// </BrowserRouter>
